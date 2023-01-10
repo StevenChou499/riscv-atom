977b3385bbf7cde7434c8845dd3afd5750cc6f87
StevenChou499 <stevenchou499@gmail.com>
2023-01-10 11:22:14 +0800

    Add testing shell script

--- riscv_test.sh
+++ riscv_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-test_files = {add addi and andi auipc beq bge bgeu blt bltu bne jal jalr lb lbu lh lhu lui lw or ori sb sh simple sll slli slt slti sltiu sltu sra srai srl srli sub sw xor xori}
+test_files=("add" "addi" "and" "andi" "auipc" "beq" "bge" "bgeu" "blt" "bltu" "bne" "jal" "jalr" "lb"  \
+              "lbu" "lh" "lhu" "lui" "lw" "or" "ori" "sb" "sh" "simple" "sll" "slli" "slt" "slti" "sltiu"\
+              "sltu" "sra" "srai" "srl" "srli" "sub" "sw" "xor" "xori")
 
-foreach test_file (test_files)
-    echo $test_file
-end
+for test_file in ${test_files[@]}; do
+    echo "testing instruction ${test_file}"
+    atomsim ../riscv-tests/isa/rv32ui-p-${test_file}
+done
