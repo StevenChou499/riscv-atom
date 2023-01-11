34938044d6780c5a8d8801d893f9cfc08e6f53da
StevenChou499 <stevenchou499@gmail.com>
2023-01-11 17:21:16 +0800

    Add RV32M instruction test

--- riscv_test.sh
+++ riscv_test.sh
@@ -4,7 +4,14 @@ test_files=("add" "addi" "and" "andi" "auipc" "beq" "bge" "bgeu" "blt" "bltu" "b
               "lbu" "lh" "lhu" "lui" "lw" "or" "ori" "sb" "sh" "simple" "sll" "slli" "slt" "slti" "sltiu"\
               "sltu" "sra" "srai" "srl" "srli" "sub" "sw" "xor" "xori")
 
+test_mul_files=("mul" "mulh" "mulhu" "mulhsu" "div" "divu" "rem" "remu")
+
 for test_file in ${test_files[@]}; do
     echo "testing instruction ${test_file}"
     atomsim ../riscv-tests/isa/rv32ui-p-${test_file}
+done
+
+for test_mul_file in ${test_mul_files[@]}; do
+    echo "testing instruction ${test_mul_file}"
+    atomsim ../riscv-tests/isa/rv32um-p-${test_mul_file}
 done
